0ac73bfc65fbd08066ab5d74c515be332574aaa4
jeppe9821 <contact.jesper.bystrom@gmail.com>
2025-07-12 00:31:35 +0200

    added log sender and reciever

--- src/app.rs
+++ src/app.rs
@@ -1,27 +1,22 @@
 use std::path::{Path, PathBuf};
-use std::thread::spawn;
 
-use crate::core::ytdownload;
-
-use iced::{Application, Length};
+use iced::{Length};
 use iced::widget::{Column, column, row, text_input, button, text, scrollable};
 use iced::widget::scrollable::{Id};
 use iced::Task;
 use std::fs;
 
-//use std::process::Command as ProcessCommand;
-
 use tokio::io::{BufReader, AsyncBufReadExt};
 use tokio::process::Command as ProcessCommand;
 use std::process::{Stdio};
 
+use std::sync::{Arc, RwLock};
+
 use crate::binary_inc;
 
-pub struct ApplicationState {
-    pub logs: Vec<String>,
-    yt_url: String,
-    directory_path: String,
-}
+use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
+
+pub type SharedLogs = Arc<RwLock<Vec<String>>>;
 
 #[derive(Debug, Clone)]
 pub enum Message {
@@ -32,34 +27,47 @@ pub enum Message {
     DownloadFailed(String),
     CopyLogsPressed,
     ClearLogsPressed,
+    LogUpdated(String),
 }
 
 const LOGS_SCROLL_ID: &str = "logs_scroll";
 
+#[derive(Clone)]
+pub struct ApplicationState {
+    yt_url: String,
+    directory_path: String,
+    state: SharedLogs,
+    log_sender: Option<UnboundedSender<Message>>,
+}
+
 pub trait App {
     fn new(_flags: ()) -> (ApplicationState, Task<Message>);
+    async fn install_ffmpeg() -> PathBuf;
     async fn install_ytdlp() -> PathBuf;
-    async fn download() -> Result<String,String>;
+    async fn download(state: SharedLogs, directory_path: String, yt_url: String, log_sender: UnboundedSender<Message>) -> Result<String,String>;
     fn update(&mut self, message: Message) -> Task<Message>;
     fn view(&self) -> Column<Message>;
 }
 
-/*fn install_ffmpeg() -> () {
-    let ffmpeg_path = binary_inc::save_ffmpeg_to_file()
-        .unwrap();
-
-    if let Ok(output) = ProcessCommand::new(ffmpeg_path).arg("-version").output() {
-        if output.status.success() {
-            let prnt: String = String::from_utf8(output.stdout)
-                .unwrap();
-
-            println!("ffmpeg is running on version {prnt}");
-        }
-    }
-}*/
-
 impl App for ApplicationState {
     fn new(_flags: ()) -> (Self, Task<Message>) {
+        let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
+        let shared_logs: SharedLogs = Arc::new(RwLock::new(Vec::new()));
+        let logs_clone = shared_logs.clone();
+
+        // Create task to consume messages and push to logs
+        let log_forward_task = Task::perform(async move {
+            tokio::spawn(async move {
+                while let Some(msg) = rx.recv().await {
+                    if let Message::LogUpdated(log) = msg {
+                        logs_clone.write().unwrap().push(log);
+                    }
+                }
+            });
+
+            Message::LogUpdated(String::new()) // dummy result
+        }, |_| Message::LogUpdated(String::new()));
+
         
         let music_dir: String = dirs::home_dir()
             .unwrap_or(PathBuf::new())
@@ -70,60 +78,87 @@ impl App for ApplicationState {
 
         (
             Self {
-                logs: Vec::new(),
                 yt_url: String::new(),
-                directory_path: music_dir
+                directory_path: music_dir,
+                state: shared_logs,
+                log_sender: Some(tx),
             },
-            Task::none(),
+            log_forward_task
         )
     }
 
-    async fn install_ytdlp() -> PathBuf {
-        let ytdlp_path = binary_inc::save_ytdlp_to_file()
-            .unwrap();
-
-        let mut command = ProcessCommand::new(&ytdlp_path);
-        command.arg("--version");
-        command.stderr(Stdio::piped());
-
-        let mut child = command.spawn()
-            .expect("Failed to spawn process");
-
-        let stderr = child.stderr.take()
-            .expect("Process did not have any errors");
-
+    async fn install_ffmpeg() -> PathBuf {
+        let ffmpeg_path = binary_inc::save_ffmpeg_to_file().unwrap();
+        let mut command = ProcessCommand::new(&ffmpeg_path);
+        command.arg("-version").stderr(Stdio::piped());
+        let mut child = command.spawn().expect("Failed to spawn process");
+        let stderr = child.stderr.take().expect("No stderr");
         let mut stderr_reader = BufReader::new(stderr).lines();
 
         tokio::spawn(async move {
-            let status = child.wait().await
-                .expect("Process encountered an error");
-            println!("Process status was: {}", status);
+            let status = child.wait().await.expect("Process error");
+            println!("Process status: {}", status);
         });
 
-        while let Some(line) = stderr_reader.next_line().await.unwrap_or_else(|e| {
-            eprintln!("Failed to read line: {}", e);
-            None
-        }) {
-            println!("Stderr line: {}", line);
+        while let Some(line) = stderr_reader.next_line().await.unwrap_or(None) {
+            println!("Stderr: {}", line);
+        }
+
+        ffmpeg_path
+    }
+
+    async fn install_ytdlp() -> PathBuf {
+        let ytdlp_path = binary_inc::save_ytdlp_to_file().unwrap();
+        let mut command = ProcessCommand::new(&ytdlp_path);
+        command.arg("--version").stderr(Stdio::piped());
+        let mut child = command.spawn().expect("Failed to spawn process");
+        let stderr = child.stderr.take().expect("No stderr");
+        let mut stderr_reader = BufReader::new(stderr).lines();
+
+        tokio::spawn(async move {
+            let status = child.wait().await.expect("Process error");
+            println!("Process status: {}", status);
+        });
+
+        while let Some(line) = stderr_reader.next_line().await.unwrap_or(None) {
+            println!("Stderr: {}", line);
         }
 
         ytdlp_path
     }
 
-    async fn download() -> Result<String,String> {
-        //self.logs.push("Installing yt-dlp".to_owned());
+    async fn download(state: SharedLogs, directory_path: String, yt_url: String, log_sender: UnboundedSender<Message>) -> Result<String,String> {
+        {
+            let mut logs = state.write().unwrap();
+            //logs.push("Installing yt-dlp...".into());
+            let _ = log_sender.send(Message::LogUpdated("Installing yt-dlp...".into()));
+        }
 
-        let ytdlp_path: PathBuf = ApplicationState::install_ytdlp().await;
+        let ytdlp_path = ApplicationState::install_ytdlp().await;
 
-        let display = ytdlp_path.display();
+        {
+            let msg = format!("yt-dlp installed at {}", ytdlp_path.display());
+            //state.write().unwrap().push(msg.clone());
+            let _ = log_sender.send(Message::LogUpdated(msg));
+        }
 
-        println!("YDLP path: {display}");
+        {
+            //state.write().unwrap().push("Installing ffmpeg...".into());
+            let _ = log_sender.send(Message::LogUpdated("Installing ffmpeg...".into()));
+        }
 
-        /*
-        let download_dir: PathBuf = Path::new(&self.directory_path).to_path_buf();
-        let output_template: PathBuf = download_dir.join("%(title)s.%(ext)s");
+        let ffmpeg_path = ApplicationState::install_ffmpeg().await;
 
-        if Path::exists(download_dir.as_path()) == false {
+        {
+            let msg = format!("ffmpeg installed at {}", ffmpeg_path.display());
+            //state.write().unwrap().push(msg.clone());
+            let _ = log_sender.send(Message::LogUpdated(msg));
+        }
+
+        let download_dir = Path::new(&directory_path).to_path_buf();
+        let output_template = download_dir.join("%(title)s.%(ext)s");
+
+        if !download_dir.exists() {
             let _ = fs::create_dir(&download_dir);
         }
 
@@ -136,55 +171,67 @@ impl App for ApplicationState {
             .arg("--continue")
             .arg("--no-abort-on-error")
             .arg("-o").arg(output_template.to_string_lossy().to_string())
-            .arg(&self.yt_url)
-            .current_dir(download_dir);
-        
-        command.spawn()
-            .expect("Failed to start");*/
+            .arg(yt_url)
+            .stdout(Stdio::piped())
+            .current_dir(&download_dir);
 
-        Ok("Result goes here".to_string())
+        let mut process = command.spawn().expect("Failed to start process");
+        let stdout = process.stdout.take().expect("No stdout");
+        let mut stdout_reader = BufReader::new(stdout).lines();
+
+        while let Some(line) = stdout_reader.next_line().await.unwrap_or(None) {
+            /*{
+                state.write().unwrap().push(line.clone());
+            }*/
+            let _ = log_sender.send(Message::LogUpdated(line.clone()));
+
+            let dis = line.clone();
+
+            println!("{dis}");
+        }
+
+        Ok("Download finished".into())
     }
 
     fn update(&mut self, message: Message) -> Task<Message> {
         match message {
             Message::DirectoryPathChanged(str) => {
-                self.directory_path = str.clone();
+                self.directory_path = str;
                 Task::none()
             }
             Message::YoutubeUrlChanged(str) => {
-                self.yt_url = str.clone();
+                self.yt_url = str;
                 Task::none()
             }
             Message::DownloadYt => {
+                let shared = self.state.clone();
+                let directory_path = self.directory_path.clone();
+                let yt_url = self.yt_url.clone();
+                let sender = self.log_sender.clone().unwrap();
+
                 Task::perform(async move {
-                    ApplicationState::download().await
-                }, | result| {
-                    match result {
-                        Ok(_) => Message::Completed,
-                        Err(e) => Message::DownloadFailed(e),
-                    }
+                    ApplicationState::download(shared, directory_path, yt_url, sender).await
+                }, |result| match result {
+                    Ok(_) => Message::Completed,
+                    Err(e) => Message::DownloadFailed(e),
                 })
             },
             Message::Completed => {
-                println!("The files has been downloaded correctly");
+                println!("Download completed");
                 Task::none()
             },
             Message::DownloadFailed(error) => {
-                println!("The download just failed! {error}");
+                println!("Download failed: {error}");
                 Task::none()
             },
-            Message::CopyLogsPressed => {
-                Task::none()
-            },
-            Message::ClearLogsPressed => {
-                Task::none()
-            }
+            Message::CopyLogsPressed => Task::none(),
+            Message::ClearLogsPressed => Task::none(),
+            Message::LogUpdated(_) => Task::none()
         }
     }
 
     fn view(&self) -> Column<Message> {
         let title = text("YouTube Playlist Downloader").size(30);
-    
         let save_directory = text_input("Save Directory", &self.directory_path)
             .on_input(Message::DirectoryPathChanged)
             .padding(10)
@@ -194,48 +241,33 @@ impl App for ApplicationState {
             .on_input(Message::YoutubeUrlChanged)
             .padding(10)
             .size(20);
-    
+
         let download_button = button("Download")
             .on_press(Message::DownloadYt)
             .padding(10)
             .style(red_button_style);
-    
-    
-        let action_button_row = row![
-            download_button,
-        ]
-        .spacing(10);
-    
+
+        let action_button_row = row![download_button].spacing(10);
+
         let copy_logs_button = button("Copy Logs")
-        .style(red_button_style)
-        .on_press(Message::CopyLogsPressed)
-        .padding(10);
+            .style(red_button_style)
+            .on_press(Message::CopyLogsPressed)
+            .padding(10);
 
         let clear_logs_button = button("Clear Logs")
-        .style(red_button_style)
-        .on_press(Message::ClearLogsPressed)
-        .padding(10);
-    
-        let log_button_row = row![
-            copy_logs_button,
-            clear_logs_button
-        ]
-        .spacing(10);
-    
-        let logs_text = self.logs.join("\n");
-        
-        let logs_display = text(logs_text)
-            .size(14)
-            .font(iced::Font::MONOSPACE);
-    
-        let scroll = scrollable(
-            column![
-                logs_display,
-            ]
-        )
-        .height(Length::Fill)
-        .id(Id::new(LOGS_SCROLL_ID));
-    
+            .style(red_button_style)
+            .on_press(Message::ClearLogsPressed)
+            .padding(10);
+
+        let log_button_row = row![copy_logs_button, clear_logs_button].spacing(10);
+
+        let logs_text = self.state.read().unwrap().join("\n");
+        let logs_display = text(logs_text).size(14).font(iced::Font::MONOSPACE);
+
+        let scroll = scrollable(column![logs_display])
+            .height(Length::Fill)
+            .id(Id::new(LOGS_SCROLL_ID));
+
         column![
             title,
             save_directory,
@@ -249,8 +281,7 @@ impl App for ApplicationState {
         .spacing(15)
         .into()
     }
-} 
-
+}
 
 fn red_button_style(_theme: &iced::Theme, _status: iced::widget::button::Status) -> iced::widget::button::Style {
     iced::widget::button::Style {
